5ba3b384fb3ba4802f59f9ec1811b489c79b8bda
zmee99 <131580626+zmee99@users.noreply.github.com>
2023-04-24 16:09:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,3 @@ client.on("ready", () => {
 ///
 client.login(process.env.token)
 
-//https://ra3dstudio.com CopyRight Codes
-
-//كيف تسوي بوت وبردكاست في سطرين فقط بدون برمجة اكواد! | how to make boardcast without coding js
-const sphinx = require("sphinx-bc"); // npm i sphinx-bc@latest - I Do updates everyday make sure you check .
-
-new sphinx.BotAccount({
-    ownerID: ["991652820203876412", "856842798212710401"],
-    prefix: "+",
-    embedReply: "Optional Option.",
-    mention: boolean
-    type: types
-}).botbc()
-//https://ra3dstudio.com CopyRight Codes
