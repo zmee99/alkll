a66329d2b07b819ab07474037523cf4be00ad3e8
zmee99 <131580626+zmee99@users.noreply.github.com>
2023-04-24 16:01:47 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+//كيف تسوي بوت وبردكاست في سطرين فقط بدون برمجة اكواد! | how to make boardcast without coding js
+const sphinx = require("sphinx-bc"); // npm i sphinx-bc@latest - I Do updates everyday make sure you check .
+
+new sphinx.BotAccount({
+    token: "YourToken",
+    ownerID: ["First Owner ID", "SECOND owner ID"],
+    prefix: "yourPrefix",
+    embedReply: "Optional Option.",
+    mention: boolean
+    type: types
+}).botbc()
+//https://ra3dstudio.com CopyRight Codes
