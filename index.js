6a0d6245f8ee46dfeaf246c36669b07b0bb7de32
zmee99 <131580626+zmee99@users.noreply.github.com>
2023-04-24 16:05:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const sphinx = require("sphinx-bc"); // npm i sphinx-bc@latest - I Do updates everyday make sure you check .
 
 new sphinx.BotAccount({
-    token: "YourToken",
+    token: "MTAyMDU2MzE5OTU5MTk4MTA2Ng.GW_7fk.yFEPqQYK6xjO_J-alp3zuTi3ASP7nMxUlnsS84",
     ownerID: ["991652820203876412", "856842798212710401"],
     prefix: "+",
     embedReply: "Optional Option.",
